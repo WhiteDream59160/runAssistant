0e3ca5dd3878673f57bdd108f96045df24cad710
WhiteDream59160 <thomas2vlieger@gmail.com>
2021-10-05 19:24:55 +0200

    XML ok moins les tests

--- src/runAssistant/runAssistant.java
+++ src/runAssistant/runAssistant.java
@@ -22,39 +22,7 @@ import org.xml.sax.SAXException;
 
 public class runAssistant {
 	
-	private static void prettyPrint(Node node, String tab)
-    {
-        if (node.getNodeType() == Node.TEXT_NODE)
-        {
-            System.out.print(tab);
-            System.out.println(node.getNodeValue());
-        }
-        else if (node.getNodeType() == Node.ELEMENT_NODE)
-        {
-            System.out.print(tab);
-            System.out.println("<" + node.getNodeName() + ">");
-            NodeList kids = node.getChildNodes();
-            for (int i = 0; i < kids.getLength(); i++)
-            {
-                prettyPrint(kids.item(i), tab + "  ");
-            }
-            System.out.print(tab);
-            System.out.println("</" + node.getNodeName() + ">");
-        }
-    }
-	
 	private static final String WINDOWXML = "windows.xml";
-	
-	private static String nodeToString(Node node) throws IOException, TransformerException
-	{
-		TransformerFactory tf = TransformerFactory.newInstance();
-		Transformer transformer = tf.newTransformer();
-		transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-		
-		StringWriter writer = new StringWriter();
-		transformer.transform(new DOMSource(node), new StreamResult(writer));
-		return writer.toString();
-	}
 	
 	public static void main(String[] args) {
 		System.out.println("Hello World");
@@ -64,29 +32,12 @@ public class runAssistant {
 
 	      try {
 
-	          // optional, but recommended
-	          // process XML securely, avoid attacks like XML External Entities (XXE)
-	          //dbf.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
-
 	          // parse XML file
 	          DocumentBuilder db = dbf.newDocumentBuilder();
 
 	          Document doc = db.parse(new File(WINDOWXML));
-
-	          // optional, but recommended
-	          // http://stackoverflow.com/questions/13786607/normalization-in-dom-parsing-with-java-how-does-it-work
+	          
 	          doc.getDocumentElement().normalize();
-
-	          System.out.println("Root Element :" + doc.getDocumentElement().getNodeName());
-	          System.out.println("------");
-
-	          // get <staff>
-	          /*
-	          NodeList list = doc.getElementsByTagName("leftSidePanel");
-			
-	          String lsp_width = "";
-	          String lsp_height = "";
-	          */
 	          
 	          NodeList lspNodeList = doc.getElementsByTagName("leftSidePanel");
 	          Node lspNode = lspNodeList.item(0);
@@ -94,14 +45,6 @@ public class runAssistant {
 
 	          String lsp_width = lspElement.getElementsByTagName("lsp_width").item(0).getTextContent();
 	          String lsp_height = lspElement.getElementsByTagName("lsp_height").item(0).getTextContent();
-	          
-	          int x = 0;
-	          int y = 0;
-	          int height = 0;
-	          int width = 0;
-	          String name = "";
-	          String desctription = "";
-	          String target = "";
 	          
 	          
 	          NodeList lspButtonList = lspElement.getElementsByTagName("button");
@@ -119,24 +62,6 @@ public class runAssistant {
 	    		      System.out.println("target : " + button.getElementsByTagName("target").item(0).getTextContent());
 	        	  }
 	          }
-	          
-	          
-	          /*
-	          for (int i = 0; i < list.getLength(); i++) {
-	        	  Node node = list.item(i);
-	        	  if (node.getNodeType() == Node.ELEMENT_NODE) {
-	        		  Element element = (Element) node;
-		        	  lsp_width = element.getElementsByTagName("lsp_width").item(0).getTextContent();
-		        	  lsp_height = element.getElementsByTagName("lsp_height").item(0).getTextContent();
-	        	  }
-	        	  
-	        	  NodeList buttons = node.getChildNodes();
-	        	  for (int j = 0)
-	          }
-	          */
-
-		      System.out.println("lsp_width : " + lsp_width);
-		      System.out.println("lsp_height : " + lsp_height);
 	          
 
 	      } catch (ParserConfigurationException | SAXException | IOException e) {
