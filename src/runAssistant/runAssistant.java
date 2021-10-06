3cd67f95e2dfddc7aacea84a46c763a89d1ae3ad
WhiteDream59160 <thomas2vlieger@gmail.com>
2021-10-06 07:58:26 +0200

    premier design gui

--- src/runAssistant/runAssistant.java
+++ src/runAssistant/runAssistant.java
@@ -2,17 +2,14 @@ package runAssistant;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.StringWriter;
 
+import java.awt.*;
+import javax.swing.*;
+//import javax.swing.JFrame;
+//import javax.swing.JPanel;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -26,6 +23,8 @@ public class runAssistant {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		
+		window();
 		
 		// Instantiate the Factory
 	      DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
@@ -68,6 +67,41 @@ public class runAssistant {
 	          e.printStackTrace();
 	      }
 
+	}
+	
+	public static void window() {
+
+		
+		JPanel leftSidePanel = new JPanel();
+		leftSidePanel.setBackground(Color.red);
+		leftSidePanel.setBounds(0, 0, 250, 500);
+		leftSidePanel.setLayout(null);
+		
+		JLabel menu1 = new JLabel();
+		menu1.setText("Menu 1");
+		menu1.setBounds(10, 10, 230, 50);
+		menu1.setBackground(Color.green);
+		menu1.setOpaque(true);
+		menu1.setVerticalAlignment(SwingConstants.CENTER);
+		menu1.setHorizontalAlignment(SwingConstants.CENTER);
+		
+		leftSidePanel.add(menu1);
+		
+		JPanel mainPanel = new JPanel();
+		mainPanel.setBackground(Color.blue);
+		mainPanel.setBounds(250, 0, 810, 500);
+		
+		
+		
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLayout(null);
+		frame.setSize(1060,500);
+		frame.setVisible(true);
+
+		frame.add(leftSidePanel);
+		frame.add(mainPanel);
+		
 	}
 
 }
